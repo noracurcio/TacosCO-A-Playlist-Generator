9a7bac392f4342e3cf04ffa9aa9d00aeca5c7b26
noracurcio <noracurcio@gmail.com>
2020-10-08 12:28:29 -0600

    pre-merge commit.

--- carousel.js
+++ carousel.js
@@ -1,7 +1,7 @@
 var apiKey = "444d1a61f4mshd57eb459b8b0709p1e745ajsn2ac16a025434"
 
 //make ajax to try to pull img from playlists into an array
-
+//This is the throwback Playlist
 var throwbackPlaylist = {
 	"async": true,
 	"crossDomain": true,
@@ -13,21 +13,30 @@ var throwbackPlaylist = {
 	}
 }
 
+
+var currentClick = 
+
+
 $.ajax(throwbackPlaylist).done(function (response) {
     console.log(response);
-    
-    // var img = $("<img>").attr("src", "https://api.deezer.com/playlist/2532117644/image" + response.md5_image + ".jpg")
-
-    // $("<img>").append(img)
-
-
-    // var img = (response.picture)
-    // console.log(img)
-
-    // $("<img>").append(img)
- 
-
     $("#picture-1").on("click", function(){
+		event.preventDefault();
+		var playistInfo = $("<li>").attr("class", "list-group-item liTag");
+		
+		$("#carousel")
+		.append(`<img src=${response.data[i].album.cover}> `)
+		.append(`<strong>Song:</strong> ${response.data[i].title_short} `)
+		.append(`<strong>Album:</strong> ${response.data[i].album.title} `)
+		.append(` <strong>Artist:</strong> ${response.data[i].artist.name}`)
+		$()
+
+
+
+	})
+
+
+	$("#picture-1").on("click", function (event) {
+    var currentClick = $("#picture-1").on("click", function(){
     var playlistOneInfo = $("<p>").text(response.title, response.tracklist)
     console.log(playlistOneInfo)
 
@@ -37,12 +46,72 @@ $.ajax(throwbackPlaylist).done(function (response) {
 });
 
 
+// $("#picture-1").on("click", function (event) {
+	
+
+	// var searchResults = userSearch;
+	// // var listEl = $("<ul>").attr("id", "ulTag");
+	// var artistList = $("<li>").attr("class", "list-group-item liTag");
+	// // var playBtn = $("<button>").attr("class", "playBtn").text("⏵");
+	// var playBtn = $("<button>").attr("class", "playBtn");
+	// playBtn
+	//   .append($("<audio controls >"))
+	//   .append(
+	// 	$(`<source src=${response.data[i].preview} type="audio/mpeg"> `)
+	//   );
+	// var addPlaylistBtn = $("<button>")
+	//   .attr("class", "addPlaylistBtn")
+	//   .text("+");
+	// $("#search-results").append(
+	//   artistList
+	// 	.append(`<img src=${response.data[i].album.cover}> `)
+	// 	.append(`<strong>Song:</strong> ${response.data[i].title_short} `)
+	// 	.append(`<strong>Album:</strong> ${response.data[i].album.title} `)
+	// 	.append(` <strong>Artist:</strong> ${response.data[i].artist.name}`)
+	// 	.append(playBtn)
+	// 	.append(addPlaylistBtn)
+	// );
+
+	
+	
+	//   var resultList = response.data.length;
+	//   searchArr = [];
+	//   for (i = 0; i < resultList; i++) {
+	// 	let searchObj = {
+	// 	  artist: response.data[i].artist.name,
+	// 	  album: response.data[i].album.title,
+	// 	  songName: response.data[i].title_short,
+	// 	  length: parseInt(response.data[i].duration),
+	// 	  albumArtwork: response.data[i].album.cover_small,
+	// 	};
+  
+	// 	var searchResults = userSearch;
+	// 	// var listEl = $("<ul>").attr("id", "ulTag");
+	// 	var artistList = $("<li>").attr("class", "list-group-item liTag");
+	// 	// var playBtn = $("<button>").attr("class", "playBtn").text("⏵");
+	// 	var playBtn = $("<button>").attr("class", "playBtn");
+	// 	playBtn
+	// 	  .append($("<audio controls >"))
+	// 	  .append(
+	// 		$(`<source src=${response.data[i].preview} type="audio/mpeg"> `)
+	// 	  );
+	// 	var addPlaylistBtn = $("<button>")
+	// 	  .attr("class", "addPlaylistBtn")
+	// 	  .text("+");
+	// 	$("#search-results").append(
+	// 	  artistList
+	// 		.append(`<img src=${response.data[i].album.cover}> `)
+	// 		.append(`<strong>Song:</strong> ${response.data[i].title_short} `)
+	// 		.append(`<strong>Album:</strong> ${response.data[i].album.title} `)
+	// 		.append(` <strong>Artist:</strong> ${response.data[i].artist.name}`)
+	// 		.append(playBtn)
+	// 		.append(addPlaylistBtn)
+	// 	);
 
 
 
 
-
-
+//This is the workout playlist
 var workoutPlaylist = {
 	"async": true,
 	"crossDomain": true,
@@ -61,8 +130,10 @@ $.ajax(workoutPlaylist).done(function (response) {
 }); 
 
 
-3126664682
 
+
+
+//This is the rock playlist 
 var rockPlaylist = {
 	"async": true,
 	"crossDomain": true,
